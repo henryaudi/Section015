4a29072f6dd856d9025082140abc434bd28f56de
Shangjie Zheng <shangjiez@mail.smu.edu>
2024-08-26 07:39:19 -0600

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Account.cpp 	new file:   Account.h 	new file:   main.cpp 	new file:   main.exe

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int num {100};
+
+  cout << "Hello from project one!" << endl;
+
+  cout << "Enter a number: ";
+  cin >> num;
+  cout << "You have just entered " << num << endl;
+  
+  return 0;
+}
